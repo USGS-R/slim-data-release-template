accc7d63236d02ace11cb05aa40482092edeb9d2
Jordan S Read <jread@usgs.gov>
2020-11-10 09:03:21 -0600

    adding initial example files and functions

---
+++ src/file_utils.R
@@ -0,0 +1,17 @@
+
+
+sf_to_zip <- function(zip_filename, sf_object, layer_name){
+  cdir <- getwd()
+  on.exit(setwd(cdir))
+  dsn <- tempdir()
+  
+  sf::st_write(sf_object, dsn = dsn, layer = layer_name, driver="ESRI Shapefile", delete_dsn=TRUE) # overwrites
+  
+  files_to_zip <- data.frame(filepath = dir(dsn, full.names = TRUE), stringsAsFactors = FALSE) %>%
+    mutate(filename = basename(filepath)) %>%
+    filter(str_detect(string = filename, pattern = layer_name)) %>% pull(filename)
+  
+  setwd(dsn)
+  zip(file.path(cdir, zip_filename), files = files_to_zip)
+  setwd(cdir)
+}
